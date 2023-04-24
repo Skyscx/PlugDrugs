508a2a6ab5c689b36ceb0c506d06f5f2958ea684
PlugDrugs <danakis20@gmail.com>
2023-04-24 22:06:00 +0300

    last ver 1.0.0.2

--- src/main/java/me/skyscx/sunland/plugdrugs/Assets/Drugs/Drugs_Items.java
+++ src/main/java/me/skyscx/sunland/plugdrugs/Assets/Drugs/Drugs_Items.java
@@ -1,12 +1,9 @@
-package me.skyscx.sunland.plugdrugs.Items;
+package me.skyscx.sunland.plugdrugs.Assets.Drugs;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
@@ -14,10 +11,9 @@ import java.util.List;
 
 import static me.skyscx.sunland.plugdrugs.Assets.Messages.*;
 
-public class CreateDrugs {
+public class Drugs_Items {
     public static ItemStack drugs_cocaine;
-    public static void init(){create_drugs_cocaine();}
-    private static void  create_drugs_cocaine(){
+    private static void  cocaine_item() {
         ItemStack drug_1 = new ItemStack(Material.WHITE_DYE, 1);
         ItemMeta meta_drug_1 = drug_1.getItemMeta();
         meta_drug_1.setDisplayName(D_001);
@@ -29,14 +25,5 @@ public class CreateDrugs {
         meta_drug_1.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         drug_1.setItemMeta(meta_drug_1);
         drugs_cocaine = drug_1;
-
-        ShapedRecipe drug1_recipe = new ShapedRecipe(NamespacedKey.minecraft("drugs_cocaine"), drug_1);
-        drug1_recipe.shape("ABA","ACA","DED");
-        drug1_recipe.setIngredient('A', Material.SUGAR);
-        drug1_recipe.setIngredient('B', Material.DEAD_BUSH);
-        drug1_recipe.setIngredient('C', Material.DIAMOND_BLOCK);
-        drug1_recipe.setIngredient('D', Material.BLAZE_POWDER);
-        drug1_recipe.setIngredient('E', Material.SUGAR_CANE);
-        Bukkit.getServer().addRecipe(drug1_recipe);
     }
 }
